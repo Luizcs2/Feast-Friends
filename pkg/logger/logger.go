c281d83620e8485028d6210865858d6348f306ea
Luizcs2 <luizohto2012@gmail.com>
2025-09-18 17:42:14 +0100

    added log for dev/production

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,35 +1,64 @@
 // TODO - Luiz
-// 3. Add log formatting for development vs production
 // 4. File rotation for log files
 // 5. Export logger functions: Info, Error, Debug, Warn	
+
+// Package logger provides a centralized logging setup for the Feast Friends API,
+// configuring log formatting and levels based on environment and application settings.
 package logger
 
 import (
-	"log"
 	"github.com/sirupsen/logrus"
 	"feast-friends-api/internal/config"
 )
 
 var Log = logrus.New()
 
+// init configures the global logger instance (Log) based on application configuration,
+// setting the log level and formatting according to the environment (development or production).
+// It is automatically invoked when the package is imported.
 func init() {
+	
+	// load config, get log level and env
+	cfg := config.Get()
+	levelString := cfg.Logging.Level
+	env := cfg.Environment
 
-	// get the log level string fron the config 
-	levelString := config.Get().Logging.Level
+	// here we set up formatting for dev or production based on environment string
+	if env == "development" {
+		Log.SetFormatter(&logrus.TextFormatter{
+			ForceColors:    true,
+			FullTimestamp: true,
+			PadLevelText:  true,
+			TimestampFormat: "15:04:05",
+		})
+		Log.SetReportCaller(true)
+	} else {
+		Log.SetFormatter(&logrus.JSONFormatter{
+			PrettyPrint: false, 
+		})
+		Log.SetReportCaller(false)
+
+	}
 
 	// try to parse the string into logrus level
 	level, err := logrus.ParseLevel(levelString)
 
 	// if parsing fails log a warning and use default level	
-	if err != nil{
-		log.Printf("Wrong LOG_LEVEL: %s , Error:%v ", levelString, err)
+	if err != nil {
+		Log.WithFields(logrus.Fields{
+			"provided": levelString,
+			"error":    err,
+		}).Warn("Invalid log level provided, falling back to info")
 		level = logrus.InfoLevel
 	}
 
 	// set final level 
 	Log.SetLevel(level)
 
-	Log.Info("Logger has been init")
+	Log.WithFields(logrus.Fields{
+		"env" : env,
+		"level": level,
+	}).Info("logger initialized")
 
 }
 
