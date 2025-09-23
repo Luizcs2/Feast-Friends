5f2c521cb21a3f2aa7e759b8df50eb33558837fc
Luizcs2 <luizohto2012@gmail.com>
2025-09-23 15:04:13 +0100

    helpers made for response

--- internal/utils/response.go
+++ internal/utils/response.go
@@ -8,10 +8,44 @@
 // 3. HTTP status code constants
 // 4. Response pagination metadata
 
-packack utils
+package utils
 
 import (
-
+	"feast-friends-api/pkg/logger"
 )
 
+
+func SuccessResponse(data interface{}, message string) map[string]interface{} {
+	logger.Info("success response : %v" , message)
+
+	return map[string]interface{}{
+		"status" : "success",
+		"message" : message,
+		"data" : data,
+	}
+
+}
+
+func ErrorResponse(err error, statusCode int) map[string]interface{} {
+	logger.Error("error response : %v" , err.Error())
+
+	return map[string]interface{}{
+		"status" : "error",
+		"message" : err.Error(),
+		"code" : statusCode,
+	}
+}
+
+func PaginatedResponse(data interface{}, totalCount, page, limit int) map[string]interface{}{
+	logger.Info("paginated response : page %d limit %d total_count %d", page, limit, totalCount)
+	return map[string]interface{}{
+		"status" : "success",
+		"data" : data,
+		"meta" : map[string]interface{}{
+			"total_count" : totalCount,
+			"page" : page,
+			"limit" : limit,
+		},
+	}
+}
 
